59f3d5773e4dc0b449b0ca901431fc9e3a720e6b
cihares <84216297+cihares@users.noreply.github.com>
2021-05-16 10:14:29 +0800

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,35 @@
+
+# # 猜猜猜!!
+# import random
+# i=0
+# num = random.randint(0,9)
+#
+# while i <3:
+#     i+=1
+#     a = int(input("guess: "))
+#     if num == a:
+#         print("YOU WIN!!")
+#         print(num)
+#         break
+#     elif i == 3:
+#         print("YOU LOSE!")
+#         print(num)
+
+# #字符转换
+# num = {
+#     "1" : "一",
+#     "2" : "二",
+#     "3" : "三",
+#     "4" : "四",
+#     "5" : "五",
+#     "6" : "六",
+# }
+#
+# x = input("请输入数字（1-6）： ")
+#
+# out = ""
+#
+# for c in x:
+#     out += num.get(c)
+# print(out)
+
